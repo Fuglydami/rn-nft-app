6128e26c4ed98ba4e284f91bbead038acb015333
damoye81@gmail.com <you@example.com>
2022-04-29 10:03:43 +0100

    initial commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { SubInfo } from "./SubInfo";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDecs from "./DetailsDecs";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsBid,
+  DetailsDecs,
+};
---
+++ utils/styles.js
@@ -0,0 +1,5 @@
+export const colors = {
+  bg: "#252C4A",
+  white: "#FFFFFF",
+  lightBlue: "#528DF9",
+};
